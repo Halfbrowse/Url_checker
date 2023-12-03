2e1122dd11680ef59b6d8aed8064187a12858bc1
Eoin Byrne <eoinbye@gmail.com>
2023-12-03 23:29:54 +0100

    added sqllite db + fucntion to read etc

--- server.py
+++ server.py
@@ -1,47 +1,44 @@
 from flask import Flask, jsonify, request
-import requests
 from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__)
 CORS(app)
-# Mock database (a simple list of URLs)
-mock_db = [
-    {"id": 1, "url": "https://example.com"},
-    {"id": 2, "url": "https://openai.com"},
-    {"id": 3, "url": "https://news.ycombinator.com"},
-    {"id": 4, "url": "https://github.com"},
-    {"id": 5, "url": "https://stackoverflow.com"},
-    {"id": 6, "url": "https://www.reddit.com"},
-    {"id": 7, "url": "https://www.medium.com"},
-    {"id": 8, "url": "https://www.wikipedia.org"},
-    {"id": 9, "url": "https://www.netflix.com"},
-    {"id": 10, "url": "https://www.amazon.com"},
-    {"id": 11, "url": "https://www.google.com"},
-    {"id": 12, "url": "https://www.facebook.com"},
-    {"id": 13, "url": "https://www.twitter.com"},
-    {"id": 14, "url": "https://www.linkedin.com"},
-    {"id": 15, "url": "https://www.instagram.com"},
-    {"id": 16, "url": "https://www.pinterest.com"},
-    {"id": 17, "url": "https://www.spotify.com"},
-    {"id": 18, "url": "https://www.apple.com"},
-    {"id": 19, "url": "https://www.microsoft.com"},
-    {"id": 20, "url": "https://www.adobe.com"},
-]
+
+
+def connect_db():
+    return sqlite3.connect("admin_panel.db")
 
 
 def normalize_url(url):
-    # Remove 'www.' prefix and trailing slash for consistent matching
+    # Remove 'https://' or 'http://' prefix, 'www.' prefix, and trailing slash for consistent matching
+    if url.startswith("https://"):
+        url = url.replace("https://", "", 1)
+    elif url.startswith("http://"):
+        url = url.replace("http://", "", 1)
+
     if url.startswith("www."):
         url = url[4:]
+
     if url.endswith("/"):
         url = url[:-1]
+
     return url.lower()
 
 
 @app.route("/check", methods=["GET"])
 def check_url():
     url_to_check = normalize_url(request.args.get("url", ""))
-    url_exists = any(normalize_url(url["url"]) == url_to_check for url in mock_db)
+    conn = connect_db()
+    c = conn.cursor()
+
+    # Assuming the URLs are stored in the 'name' column of the 'Channels' table
+    c.execute("SELECT name FROM Channels")
+    channels = c.fetchall()
+
+    url_exists = any(normalize_url(channel[0]) == url_to_check for channel in channels)
+    conn.close()
+
     return jsonify({"exists": url_exists}), 200
 
 
