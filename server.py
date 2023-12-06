278dbac96864f35ba252f74ff98482f13916c526
Eoin Byrne <eoinbye@gmail.com>
2023-12-06 20:49:04 +0100

    updated server and js scripts to work with sqlite db

--- server.py
+++ server.py
@@ -11,34 +11,57 @@ def connect_db():
 
 
 def normalize_url(url):
-    # Remove 'https://' or 'http://' prefix, 'www.' prefix, and trailing slash for consistent matching
-    if url.startswith("https://"):
-        url = url.replace("https://", "", 1)
-    elif url.startswith("http://"):
-        url = url.replace("http://", "", 1)
-
-    if url.startswith("www."):
-        url = url[4:]
-
-    if url.endswith("/"):
-        url = url[:-1]
-
-    return url.lower()
+    # Modify this normalization to match how URLs are stored in your database
+    return url.lower().strip()
 
 
 @app.route("/check", methods=["GET"])
 def check_url():
-    url_to_check = normalize_url(request.args.get("url", ""))
+    url_to_check = request.args.get("url", "")
     conn = connect_db()
     c = conn.cursor()
+    print("Checking URL:", url_to_check)
 
-    # Assuming the URLs are stored in the 'name' column of the 'Channels' table
-    c.execute("SELECT name FROM Channels")
-    channels = c.fetchall()
+    # List of column names to check against
+    columns = [
+        "External_links",
+        "Website",
+        "Twitter",
+        "Twitter_ID",
+        "Facebook",
+        "Threads",
+        "YouTube",
+        "YouTube_ID",
+        "TikTok",
+        "Instagram",
+        "LinkedIn",
+        "Reddit",
+        "VK",
+        "Telegram",
+        "Substack",
+        "Quora",
+        "Patreon",
+        "GoFundMe",
+        "Paypal",
+        "Twitch",
+        "Mastadon",
+        "Wechat",
+        "QQ",
+        "Douyin",
+    ]
 
-    url_exists = any(normalize_url(channel[0]) == url_to_check for channel in channels)
+    url_exists = False
+    for column in columns:
+        query = f"SELECT {column} FROM Entities WHERE {column} = ?"
+        print("Checking column:", column)  # Print the column being checked
+        c.execute(query, (url_to_check,))
+        result = c.fetchone()
+        print("Result for", column, ":", result)  # Print the result for each column
+        if result:
+            url_exists = True
+            break
+
     conn.close()
-
     return jsonify({"exists": url_exists}), 200
 
 
